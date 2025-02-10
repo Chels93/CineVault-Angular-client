ae80b66f26900a2c2ec41fcc562e9211a313b72e
Chels93 <cacguff@gmail.com>
2025-02-10 15:09:09 -0500

    renamed getFavoriteMovies to getfavoriteMovies to be consistent throughout code

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -76,7 +76,7 @@ export class UserProfileComponent implements OnInit {
   ngOnInit(): void {
     this.checkAuthentication();
     this.getUser();
-    this.getFavoriteMovies();
+    this.getfavoriteMovies();
     this.currentRoute = this.router.url.split('/').pop() || '';
   }
 
@@ -149,7 +149,7 @@ export class UserProfileComponent implements OnInit {
   /**
    * Fetches the user's favorite movies from the API.
    */
-  private getFavoriteMovies(): void {
+  private getfavoriteMovies(): void {
     this.loading = true;
     this.fetchApiData
       .getfavoriteMovies()
